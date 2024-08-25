0639c328a46e3c4e87c1405254564c7a4e17c0ba
Cjvp200414 <150268235+Cjvp200414@users.noreply.github.com>
2024-08-25 23:15:38 +0800

    Add files via upload

--- firstActCPP.cpp
+++ firstActCPP.cpp
@@ -1,38 +1,34 @@
 #include <iostream>
 using namespace std;
 
-void listFileDetail() {
-    int choice;
-    cout << "\tLIST FILE DETAIL" << endl;
-    cout << "\t----------------------" << endl;
-    cout << "1. List All Files"<< endl;
-    cout << "2. List of Extension Files"<< endl;
-    cout << "3. List of Name Wise"<< endl;
-        cout << "Enter your choice: ";
-        cin >> choice;
+void listOfFile(){
+	int choice;
+	cout << "\t**** LIST FILE DETAILS ****\n";
+	cout << "1. List All Files \n";
+	cout << "2. List of Extension Files \n";
+	cout << "3. List of Name Wise\n";
+	cout << "Enter your Choice: ";
+		cin >> choice;
 }
 
-void listOfAllFiles() {
-    cout << "\nList of All(*.*) Files" << endl;
-    cout << "file1.txt" << endl;
-    cout << "file2.cpp" << endl;
-    cout << "report.doc" << endl;
-    cout << "image.jpg" << endl;
-    cout << "(continues...)" << endl;
-    cout << "\nTotal Files: 4"<< endl;
-    cout << "Press any key to continue...\n"<< endl;
+void listOfAllFile(){
+	cout << "\nList of All(*.*) Files \n";
+	cout << "file1.txt\nfile2.cpp\nreport.doc\nimage.jpg\n(continues...)\n";
+	cout << "\nTotal Files: 4\n";
+	cout << "Press any key  to continue...\n\n";
+	
 }
 
-void createDirecName() {
-    string direcName;
-    cout << "Enter the Directory name: "<< endl;
-    cin >> direcName;
-    cout << "Current directory: C:\\Users\\UserName\\Documents"<< endl;
-    cout << direcName << " Directory Successfully Created"<< endl;
-    cout << "Press any key to continue...\n"<< endl;
+void createDirectory(){
+	string direcName;
+	cout << "Enter the Directory name: \n";
+		cin >> direcName;
+	cout << "Current directory: C: \\Users\\UserName\\Documents\n";
+	cout << direcName << "Directory Successfully Created\n";
+	cout << "Press any key to continue...\n\n";
 }
-
-void changeDirectory() {
+ 
+ void changeDirectory() {
     string dirName;
     int choice;
     cout << "\nCurrent Directory: C:\\Users\\UserName\\Documents\n" << endl;
@@ -49,14 +45,51 @@ void changeDirectory() {
     cout << "Press any key  to continue...\n\n";
 }
 
+int main (){
+	int choice;
+    do{
+        cout << "\t**** MAIN MENU ****\n";
+        cout << "1. To Display List of Files\n";
+        cout << "2. To Create New Directory\n";
+        cout << "3. To Change the Working Directory\n";
+        cout << "4. Exit\n";
+        cout << "Enter the Number: ";
+        cin >> choice;
+  		
+     
+        if (choice == 1){
+        	listOfFile();
+            listOfAllFile();
+		}else if(choice == 2){
+			createDirectory();
+		}else if(choice == 3){
+			changeDirectory();
+		}else if(choice == 4){
+			cout << "Exiting..." << endl;
+		}else {
+			cout << "Invalid choice! Try again." << endl;
+		}
 
+    } while (choice != 4);
 
-int main() {
-    listFile();
-    listOfAllFiles();
-    createDirecName();
-    return 0;
+	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
